122a362502057e864d43d77e7daa34219027cae5
kimkill555 <kimptonmak@gmail.com>
2026-01-09 11:06:35 +0200

    Searchbar functionality halfway point - fix result duplicate display

--- MoreInfo/MoreInfo.js
+++ MoreInfo/MoreInfo.js
@@ -1,9 +1,10 @@
+import { grabFile } from "../Common.js";
+
 let HiddenInfo = "/MoreInfo/HiddenInfo.html";
-let MoreInfo = "/MoreInfo/MoreInfoPage.html";
+let infoCategories = ["Rules", "Activities", "Projects", "Events"];
 
 function preRun(){
     let headerMid = document.getElementById("HeaderMiddle");
-    let buttonOptions = ["Rules", "Activities", "Projects", "Events"];
     
     class button{
         constructor(text,func){
@@ -40,12 +41,12 @@ function preRun(){
                 break;
             ;
             default: 
-                window.alert("Invalid selection");
+                window.alert("Insecure Debugger - Invalid selection");
         }
     }
 
     function ReCompileHeader(){
-        buttonOptions.forEach((option)=>{
+        infoCategories.forEach((option)=>{
             let myButton = new button(option,()=>{
                 Reveal(option);
             });
@@ -55,22 +56,13 @@ function preRun(){
     }
 
     ReCompileHeader();
-    document.getElementById("SearchButton").onclick = (key)=>{
-        Search("hey");
+    document.getElementById("SearchButton").onclick = ()=>{
+        let key = document.getElementById("SearchBarSearch").value;
+        Search(key);
     }
 }
 
-window.setTimeout(preRun,100);
-
-//Searchbar Functionality
-//Testing out making use of JS modules
-import { grabFile } from "../Common.js";
-
-async function Search(keyword){
-    let content = await grabFile(HiddenInfo);
-    console.log(content);
-}
-
+//Function to get the "Hidden Info" and replace it with the "Visible info"
 async function ReplaceContent(topic){
     let content = await grabFile(HiddenInfo);
     let tempDoc = document.implementation.createHTMLDocument();
@@ -89,4 +81,88 @@ async function ReplaceContent(topic){
         visibleInfo.replaceChildren(contentToShow);
     }
     tempDoc = null;
+}
+
+window.setTimeout(preRun,100);
+
+//Searchbar Functionality
+
+async function Search(keyword){
+    let content = await grabFile(HiddenInfo);
+    let tempDoc = document.implementation.createHTMLDocument();
+    
+    tempDoc.body.innerHTML = content;
+    let categories = tempDoc.body.children;
+
+    for (var i=0 ; i<tempDoc.body.childElementCount;i++){ 
+        let categoryText = categories[i].textContent;
+        let index = categoryText.indexOf(keyword);
+        let category = categories[i].id;
+        let formattedText = "";
+
+        if (index != -1){
+            let spaces = spaceIndexes(categoryText,index,3,3);
+            formattedText = categoryText.substring(spaces.spaceBefore,spaces.spaceAfter)
+            console.log(spaces);
+            console.log(formattedText);
+            let result = new searchResult(formattedText,category);
+            result.showResults();
+            document.getElementById("SearchResultContainer").style = "display:block";
+        }
+    }
+}
+
+class searchResult{
+    constructor(displayText, category){
+        this.displayText = displayText;
+        this.category = category;
+    }
+
+    showResults(){
+        let parent = document.createElement("div");
+        let searchRes = document.createElement("p");
+        let categorytext = document.createElement("p");
+        searchRes.innerText = this.displayText;
+        searchRes.style = "background-color:white; color:black;";
+        searchRes.onclick = ()=>{ReplaceContent(this.category)};
+        categorytext.innerText = "from: " + this.category;
+        categorytext.style = "background-color:black; color:white;";
+        parent.append(searchRes,categorytext);
+        parent.style = "position:relative; z-index:1;";
+        document.getElementById("SearchResultContainer").append(parent);
+    }
+}
+
+//Word index is a number, spaceIndex is a dict of these numbers!
+function spaceIndexes(searchString, wordIndex, numSpacesBefore,numSpacesAfter){
+    let spaceIndex = {spaceBefore:wordIndex, spaceAfter:wordIndex};
+    
+
+    for (var i=0;i<numSpacesBefore;i++){
+        spaceIndex.spaceBefore = searchString.lastIndexOf(" ",spaceIndex.spaceBefore-1);
+        if (spaceIndex.spaceBefore < 0){
+            spaceIndex.spaceBefore=0;
+            break;
+        }
+    }
+    //backup index cause indexof will return -1 if no substr is found after the last found substr.
+    for (var i=0;i<numSpacesAfter;i++){
+        let backupIndex = spaceIndex.spaceAfter;
+        spaceIndex.spaceAfter = searchString.indexOf(" ",spaceIndex.spaceAfter+1);
+        if (spaceIndex.spaceAfter < 0){
+            spaceIndex.spaceAfter = backupIndex;
+            break;
+        }
+        else{
+            backupIndex = spaceIndex.spaceAfter;
+        }
+    }
+
+    return spaceIndex;
+}
+
+document.onclick = ()=>{
+    if (document.getElementById("SearchResultContainer")){
+        document.getElementById("SearchResultContainer").style = "Display:none";
+    }
 }
