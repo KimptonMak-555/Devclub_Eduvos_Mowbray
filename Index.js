03f499114c3e33b234240e9173ed9f7066c214ac
kimkill555 <kimptonmak@gmail.com>
2026-01-20 00:43:49 +0200

    Search functionality minor improvement with hiding search result

--- Index.js
+++ Index.js
@@ -1,6 +1,8 @@
 function switchToHome(){
     window.open("/Home/HomePage.html");
+    console.log(document.URL)
+    console.log("switched to home")
 }
 
 console.log("switchign to home in 200 ms")
-window.setTimeout(200,switchToHome);
+window.setTimeout(switchToHome,200);
--- MoreInfo/MoreInfo.js
+++ MoreInfo/MoreInfo.js
@@ -72,13 +72,14 @@ async function ReplaceContent(topic){
     let contentToShow = tempDoc.getElementById(topic);
     let origins = tempDoc.getElementById("Origins");
     let visibleInfo = document.getElementById("VisibleInfo");
-    console.log(visibleInfo.children[0]);
     
-    if (visibleInfo.children[0].getAttribute("id") == topic){
-        visibleInfo.replaceChildren(origins);
-    }
-    else{
-        visibleInfo.replaceChildren(contentToShow);
+    if (infoCategories.indexOf(topic) != -1){
+        if (visibleInfo.children[0].getAttribute("id") == topic){
+            visibleInfo.replaceChildren(origins);
+        }
+        else{
+            visibleInfo.replaceChildren(contentToShow);
+        }
     }
     tempDoc = null;
 }
@@ -93,6 +94,7 @@ async function Search(keyword){
     
     tempDoc.body.innerHTML = content;
     let categories = tempDoc.body.children;
+    let bFoundRes = false;
 
     for (var i=0 ; i<tempDoc.body.childElementCount;i++){ 
         let categoryText = categories[i].textContent;
@@ -102,31 +104,39 @@ async function Search(keyword){
 
         if (index != -1){
             let spaces = spaceIndexes(categoryText,index,3,3);
-            formattedText = categoryText.substring(spaces.spaceBefore,spaces.spaceAfter)
+            formattedText = categoryText.substring(spaces.spaceBefore,spaces.spaceAfter);
+            bFoundRes = true;
             console.log(spaces);
             console.log(formattedText);
             let result = new searchResult(formattedText,category);
             result.showResults();
-            document.getElementById("SearchResultContainer").style = "display:block";
         }
     }
+
+    if (bFoundRes == false){
+        let result = new searchResult("Query not found...","undefined");
+        result.showResults();
+    }
+
+    document.getElementById("SearchResultContainer").style = "display:block";
 }
 
 class searchResult{
     constructor(displayText, category){
         this.displayText = displayText;
         this.category = category;
+        this.parentContainer = document.getElementById("SearchResultContainer");
     }
 
     showResults(){
         let parent = document.createElement("div");
         let searchRes = document.createElement("p");
         let categorytext = document.createElement("p");
-        let parentContainer = document.getElementById("SearchResultContainer");
+        let parentContainer = this.parentContainer;
 
         searchRes.innerText = this.displayText;
         searchRes.style = "background-color:white; color:black;";
-        searchRes.onclick = ()=>{ReplaceContent(this.category)};
+        parent.onclick = ()=>{ReplaceContent(this.category)};
         
 
         categorytext.innerText = "from: " + this.category;
@@ -135,12 +145,31 @@ class searchResult{
         //By allocating a class, each of the results can be structured according to the class they are in.
         parent.class = "resFrom"+this.category;
         parent.append(searchRes,categorytext);
-
+        console.log(this.parentContainer);
         parentContainer.append(parent);
+
+
+        document.addEventListener("click",this.hideResults,{once:true})
+    }
+
+    hideResults(){
+        let parentContainer = document.getElementById("SearchResultContainer");
+        console.log(parentContainer);
+        if (parentContainer.style.display != "none"){
+            parentContainer.style = "Display:none";
+            parentContainer.replaceChildren();
+            parentContainer.value = "";
+            console.log("Deleted results")
+        }
+        else{
+            console.log("No delete results")
+        }
     }
 }
 
 //Word index is a number, spaceIndex is a dict of these numbers!
+//After minimal consideration, this will be used again to find more words in the same paragraph/search location which match the search query. 
+//THIS CAN EVEN POSSIBLY BE USED TO SEARCH A WEB DOC FOR SOMETHING -- or not
 function spaceIndexes(searchString, wordIndex, numSpacesBefore,numSpacesAfter){
     let spaceIndex = {spaceBefore:wordIndex, spaceAfter:wordIndex};
     
@@ -168,10 +197,6 @@ function spaceIndexes(searchString, wordIndex, numSpacesBefore,numSpacesAfter){
     return spaceIndex;
 }
 
-document.onclick = ()=>{
-    if (document.getElementById("SearchResultContainer")){
-        document.getElementById("SearchResultContainer").style = "Display:none";
-        document.getElementById("SearchResultContainer").replaceChildren();
-        document.getElementById("SearchBarSearch").value = "";
-    }
-}
+// document.onclick = ()=>{
+    
+// }
