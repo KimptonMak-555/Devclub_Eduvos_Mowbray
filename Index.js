fe76d10b8d059a42207455c691bebcb8355dc801
Kim <kimptonmak@gmail.com>
2026-01-23 02:09:25 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,9 +1,9 @@
 function switchToHome(){
-    let prefix= window.location.origin;
+    let prefix= window.location.href;
     window.open(prefix + "/Home/HomePage.html","_self");
     
 }
 
 console.log("switchign to home in 5000 ms");
-console.log(window.location.origin);
+console.log(window.location.href);
 window.setTimeout(switchToHome,5000);
