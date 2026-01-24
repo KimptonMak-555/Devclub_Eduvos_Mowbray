7b245736501b472549607cd07337be7684fe0e6b
kimkill555 <kimptonmak@gmail.com>
2026-01-25 00:05:44 +0200

    final resource locate fix

--- Index.js
+++ Index.js
@@ -1,8 +1,9 @@
 function switchToHome(){
-    let prefix= window.location.href;
-    window.open(prefix + "/Home/HomePage.html","_self");
+    let prefix= window.location.origin;
+    window.open("Home/HomePage.html","_self");
     
 }
 
 console.log("switchign to home in 5000 ms")
-window.setTimeout(switchToHome,5000);
+console.log(window.location.origin)
+window.setTimeout(switchToHome,2000);
